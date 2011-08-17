686febb718d6b83dc27e23c1600564f64c8f9478
Jürgen Geuter <tante@the-gay-bar.com>
2011-08-17 21:40:58 +0200

    kukukukukukubump

---
+++ planer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+#####
+# Hard Constraints:
+# - Es gibt zwei Wegpunkte: Oldenburg und Varel
+# - Wer hin fährt, muss auch zurück kommen
+# - In einem Auto sitzen 4 oder weniger Personen
+# - wenn nur eine Person im Auto ist, muss es ihres sein
+# - Jeder hat ein Auto
+# - Gefahren wird Montags bis einschl. Freitags
+# - Jeden Tag gibts Schulstunden von 1 bis 9
+# - Kein Warten nach dem Ende des Tages
+# - Kein zu früh zur Arbeit fahren
+# - Wenn ein Auto hinfährt, muss es auch wieder zurück
+#
+# Soft Constraints:
+# - Niemand sollte häufiger als 3 mal die Woche fahren
+# - Weniger Wartezeiten sind besser
+# - Dieselben Besetzungen ?
+
+# Eingabe:
+# FOR person in Personen:
+#   FOR tag in Tagen:
+#      anfahrt = Stundenzahl
+#      abfahrt = stundenzahl
+
+
