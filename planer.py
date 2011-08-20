67857aed39205a4cd52af7d4a0a92abac8b4dbd8
Jürgen Geuter <tante@the-gay-bar.com>
2011-08-20 22:38:11 +0200

    Chnages

--- planer.py
+++ planer.py
@@ -25,7 +25,7 @@
 #      abfahrt = stundenzahl
 
 import csv,pprint
-from random import choice
+from random import choice,shuffle
 
 # read data
 reader = csv.reader(open("data.csv"))
@@ -37,7 +37,7 @@ datastore = {}
 for day in days:
     hours = dict.fromkeys(range(1,10))
     for hour in hours:
-        hours[hour] = {'hin': [], 'rück': []}
+        hours[hour] = {'hin': [], 'rück': [], }
     datastore[day] = hours
 
 # now fill in the data
@@ -45,25 +45,84 @@ for row in reader:
     name = row[0]
     for i in range(1,11):
         direction = "hin" if i%2 else "rück"
-        hour = int(row[i])
-        datastore[days[int((i-1)/2)]][hour][direction].append(name)
+        try:
+            hour = int(row[i])
+            if hour!=-1:
+                datastore[days[int((i-1)/2)]][hour][direction].append(name)
+        except:
+            pass
     
-def create_groups(ori_people):
-    people = list(ori_people)
+def assign(plan,day,hour,direction):
+    people = list(plan[day][hour][direction]["people"])
+    drivers = list(plan[day][hour][direction]["drivers"])
+    groups=[]
     if not people:
         return []
+    for person in drivers:
+        groups.append({'driver':person,'people':[]})
+        people.remove(person)
+    if not people:
+        return groups
     amount = len(people)
     amount_groups = amount//4 if amount%4==0 else (amount//4)+1
-    groupsize=amount//amount_groups
-    groups=[]
-    for i in range(0,amount_groups):
-        group = {"driver": None, "people":[]}
-        for i in range(0,groupsize):
-            person = choice(people)
-            people.remove(person)
-            group['people'].append(person)
-        groups.append(group)
+    avg_size = amount//amount_groups
+    for i in range(0,amount_groups-len(drivers)):
+        groups.append({'driver':None,'people':[]})
+    # randomize list
+    shuffle(people)
+    for group in groups:
+        for i in range(0,avg_size):
+            if people:
+                group['people'].append(people.pop())
+        if not group['driver']:
+            if direction=="hin":
+                group['driver']=choice(group['people'])
+                group['people'].remove(group['driver'])
+                make_driver(group['driver'],day,plan)
+            else:
+                for person in group['people']:
+                    if is_driver(plan,person,day):
+                        group['driver']=person
+                        group['people'].remove(group['driver'])
+                        make_driver(group['driver'],day,plan)
+                if not group['driver']:
+                    group['driver']=choice(group['people'])
+                    group['people'].remove(group['driver'])
+                    make_driver(group['driver'],day,plan)
+    
     return groups         
+
+def is_driver(plan,person,day):
+    driver = False
+    for hour in plan[day].keys():
+        for group in plan[day][hour]['hin']['groups']:
+            if person==group['driver']:
+                return True
+        for group in plan[day][hour]['rück']['groups']:
+            if person==group['driver']:
+                return True
+    return driver
+
+
+
+def make_driver(person,day,plan):
+    for hour in plan[day].keys():
+        if person in plan[day][hour]["hin"]['people']:
+            plan[day][hour]["hin"]['drivers'].append(person)
+        if person in plan[day][hour]["rück"]['people']: 
+            plan[day][hour]["rück"]['drivers'].append(person)
+
+def get_avail_cars(day,time,plan):
+    cars=[]
+    for h in range(1,time+1):
+        for driver in plan[day][h]['hin']['drivers']:
+            cars.append(driver)
+        for driver in plan[day][h]['rück']['drivers']:
+            try:
+                cars.remove(driver)
+            except:
+                pass
+    return cars
 
 plan = datastore.copy()
 # now we gotta decide who drives with whom
@@ -71,27 +130,35 @@ for day, data in datastore.items():
     # make room to note drivers
     loners=set()
     for hour in data.keys():
-        plan[day][hour]['hin'] = {'people':plan[day][hour]['hin'],"drivers":[]}
-        plan[day][hour]['rück'] = {'people':plan[day][hour]['rück'],"drivers":[]}
-        plan[day][hour]['cars'] = 0
+        plan[day][hour]['hin'] = {'people':plan[day][hour]['hin'],"drivers":[],"groups":[]}
+        plan[day][hour]['rück'] = {'people':plan[day][hour]['rück'],"drivers":[],"groups":[]}
+
         # whoever has to drive alone at some point becomes a driver
         if len(plan[day][hour]['hin']['people'])==1:
             loners.add(plan[day][hour]['hin']['people'][0])
         if len(plan[day][hour]['rück']['people'])==1:
             loners.add(plan[day][hour]['rück']['people'][0])
     # now we marked to people who have to drive alone in one direction, 
-    # let's make them drivers for their other way as well
-    for hour in data.keys():
-        for person in plan[day][hour]['hin']['people']:
-            if person in loners:
-                plan[day][hour]['hin']['drivers'].append(person)
+    # they are drivers for their other way as well
+    for person in loners:
+        make_driver(person,day,plan)
 
-        for person in plan[day][hour]['rück']['people']:
-            if person in loners:
-                plan[day][hour]['rück']['drivers'].append(person)
+    # Now we gotta see if we have drivers for everything.
+    # Every group driving back needs a car there
+
+def make_plan(plan):
+    #make backup_copy
+    newplan=plan.copy()
+    for day in newplan.keys():
+        for hour in newplan["mo"].keys():
+            newplan[day][hour]['hin']['groups'] = assign(newplan,day,hour,"hin")
+            newplan[day][hour]['rück']['groups'] = assign(newplan,day,hour,"rück")
+
+    return newplan         
+        
+
+if __name__=="__main__":
+    myplan = make_plan(plan)
+    pprint.pprint(myplan)
 
 
-
-    
-
-pprint.pprint(plan)
